9a60708af0c01539163ebbd279dac9a69c980e82
fallah-rasool <fallah.rasol@gmail.com>
2022-08-12 22:47:48 +0430

    uplode ecommrce

---
+++ js/about.4b47e386.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkecommrce"]=self["webpackChunkecommrce"]||[]).push([[443],{5399:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var u=function(){var e=this;e._self._c;return e._m(0)},s=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],c=t(1001),r={},a=(0,c.Z)(r,u,s,!1,null,null,null),l=a.exports}}]);
+//# sourceMappingURL=about.4b47e386.js.map
